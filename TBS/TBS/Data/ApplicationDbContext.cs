b7be50d8780a62e9b1bf4ad6655ec36693136976
Saurabh Srivastava <srb.sri25@gmail.com>
2024-06-01 13:40:16 +0530

    Add code & inherit with base( option)

--- TBS/TBS/Data/ApplicationDbContext.cs
+++ TBS/TBS/Data/ApplicationDbContext.cs
@@ -2,8 +2,10 @@
 
 namespace TBS.Data
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext : DbContext
     {
-
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
     }
 }
