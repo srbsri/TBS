0448aae9c38bd028788791fe8e433af53e15bd8a
Saurabh Srivastava <srb.sri25@gmail.com>
2024-06-01 13:32:55 +0530

    1. Add Model class & install package mangrs

---
+++ TBS/TBS/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace TBS.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Description { get; set; }
+    }
+}
