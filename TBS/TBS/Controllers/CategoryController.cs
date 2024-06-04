060b6096d7229c6ae7445f0937082962ed7be595
Saurabh Srivastava <srb.sri25@gmail.com>
2024-06-04 12:38:19 +0530

    add code in CateCtrl to retreive data in list form from Db

--- TBS/TBS/Controllers/CategoryController.cs
+++ TBS/TBS/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TBS.Data;
+using TBS.Models;
 
 namespace TBS.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController (ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList= _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
