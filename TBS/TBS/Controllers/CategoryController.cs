2752e7c61739c0aa53b1556348503982df20f533
Saurabh Srivastava <srb.sri25@gmail.com>
2024-06-19 16:35:53 +0530

    add [HttpPost] and Create IActionResult (Category obj)

--- TBS/TBS/Controllers/CategoryController.cs
+++ TBS/TBS/Controllers/CategoryController.cs
@@ -7,14 +7,28 @@ namespace TBS.Controllers
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public CategoryController (ApplicationDbContext db)
+        public CategoryController(ApplicationDbContext db)
         {
             _db = db;
         }
         public IActionResult Index()
         {
-            List<Category> objCategoryList= _db.Categories.ToList();
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {           
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
+
